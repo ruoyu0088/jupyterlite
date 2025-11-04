dcee290247ed07d6c78f2752e090bad80af9ff14
ruoyu0088 <ruoyu0088@gmail.com>
2025-11-04 19:45:46 +0900

    ipyevents patch: disable default action for key events

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 jupyter lite build --contents content --output-dir dist --pyodide pyodide.tar.bz2
 python utils.py patch
 copy /y patch\568.01d8de54f0240b4168bd.js dist\extensions\jupyterlab-open-url-parameter\static\
+copy /y patch\122.99bdb660447bc558238a.js dist\extensions\ipyevents\static\
 python fix_content.py
